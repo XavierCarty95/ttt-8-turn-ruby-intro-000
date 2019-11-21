78ee9d836d58961f3810e1e23f53de1e760541ca
Xavier Carty <xaviercarty@gmail.com>
2019-11-21 05:46:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ end
 def valid_move?(board , index)
    if index < 8
   def position_taken?(board , index)
-    board[index] == "" || board[index] === " " || board[index] == " "
+    if board[index] == "" || board[index] === " " || board[index] == " "
      return true
    else
      return false

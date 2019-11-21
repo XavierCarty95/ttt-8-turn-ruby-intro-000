80c9223c96109c298fe7a2c5ed2435fd351feddb
Xavier Carty <xaviercarty@gmail.com>
2019-11-21 06:50:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,15 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-#coverts a user's place on the board to the index integer
+
 def input_to_index(input)
-  index = input
-  index -= 1
+  index = input -1
   return index
 end
 
-#checks the number to see if 1) the position is already taken and 2)it is an actual number
-def valid_move?(board, index)
+
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
       return false
